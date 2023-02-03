06dd88b74461abdd22e3fe15ba5c90f2ed4e9c5b
Medi King's <jehovahmbote3@gmail.com>
2023-02-03 10:21:26 +0100

    Mise à jours & responsive

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
-$(document).ready(()=>{
+$('document').ready(function(){
 
-    
+    // Admin menu responsive
+    $('#admin-menu-bars').click(()=>{
+        $('.dashboard .sidebar').toggleClass('hide show');
+        $('.dashboard .main-admin').toggleClass('full aside');
+    })
 
 })
